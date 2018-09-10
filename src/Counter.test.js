11d149a528608fa62a24d88a7ad89b07130728d7
rhys5690 <rhys5690@gmail.com>
2018-09-10 13:03:32 +1000

    tested counter

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { render, cleanup } from "react-testing-library";
-import Counter from './Counter';
+import Counter from "./Counter";
 
-test('<Counter />', () => {
+test("<Counter />", () => {
   const wrapper = render(<Counter />);
   wrapper.debug();
-})
+  expect(wrapper.getByText("0").tagName).toBe('BUTTON');
+});
