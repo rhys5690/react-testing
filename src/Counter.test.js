fb2d15ecd65a396f526b5a8becdb3d79da69cb5a
rhys5690 <rhys5690@gmail.com>
2018-09-10 10:47:15 +1000

    created test and used render

---
+++ src/Counter.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, cleanup } from "react-testing-library";
+import Counter from './Counter';
+
+test('<Counter />', () => {
+  const wrapper = render(<Counter />);
+  wrapper.debug();
+})
