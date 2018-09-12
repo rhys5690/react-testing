17344843bc2742b13bd2b46d58104f7c5e6d0347
rhys5690 <rhys5690@gmail.com>
2018-09-13 09:45:45 +1000

    added snapshot tests

--- src/NewMovie.test.js
+++ src/NewMovie.test.js
@@ -5,8 +5,9 @@ import NewMovie from './NewMovie';
 afterEach(cleanup);
 
 test('<NewMovie />', () => {
-  const { debug, getByTestId, queryByTestId } = render(<NewMovie />);
+  const { debug, getByTestId, queryByTestId, container } = render(<NewMovie />);
   expect(getByTestId('page-title').textContent).toBe('New Movie');
   expect(queryByTestId('movie-form')).toBeTruthy();
+  expect(container.firstChild).toMatchSnapshot();
   debug();
 });
